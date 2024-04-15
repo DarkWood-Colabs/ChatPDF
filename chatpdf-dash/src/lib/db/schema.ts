62df3fb124a90ac44a48ffe7f0a584afe169bf8b
NixLyn_Dev <99684970+DillonBreytenach@users.noreply.github.com>
2024-04-15 16:33:59 +0000

    ChatsTabSQL

--- chatpdf-dash/src/lib/db/schema.ts
+++ chatpdf-dash/src/lib/db/schema.ts
@@ -1,10 +1,12 @@
 import { timestamp } from 'drizzle-orm/mysql-core'
-import {pgTable} from 'drizzle-orm/pg-core'
+import {pgTable, varchar} from 'drizzle-orm/pg-core'
 
 export const chats = pgTable('chats', {
     id: serial('id').primaryKey(),
     pdfName: text('pdf_name').notNull(),
     pdfUrl: text('pdf_url').notNull(),
     createdAt: timestamp('created_at').notNull().defaultNow(),
+    userId: varchar('user_id', {length:256}).notNull(),
+    fileKey: text('file_key').notNull()
 })
 
