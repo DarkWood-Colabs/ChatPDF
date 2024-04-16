29d444ebbe9653c961d2193418d0113067252742
NixLyn_Dev <99684970+DillonBreytenach@users.noreply.github.com>
2024-04-16 06:38:25 +0000

    Schema-pgTables

---
+++ chatpdf-dash/drizzle.config.ts
@@ -0,0 +1,18 @@
+// Drizzle-Kit
+import type {Config} from 'drizzle-kit'
+//dotEnv
+import * as dotenv from 'dotenv'
+dotenv.config({ path: '.env' })
+
+
+export default {
+    driver: 'pg',
+    schema: './src/lib/db/schema.ts',
+    dbCredentials: {
+        connectionString: process.env.DATABASE_URL!,
+    }
+} satisfies Config
+
+// npx drizzle-kit push:pg  
+// ^^  -> Makes Sure Neon DB is Synced up with Schema
+
--- chatpdf-dash/src/lib/db/schema.ts
+++ chatpdf-dash/src/lib/db/schema.ts
@@ -1,5 +1,17 @@
 import { timestamp } from 'drizzle-orm/mysql-core'
-import {pgTable, varchar} from 'drizzle-orm/pg-core'
+import {
+    integer, 
+    pgEnum, 
+    pgTable, 
+    serial, 
+    text, 
+    varchar,
+    } from 'drizzle-orm/pg-core'
+
+// Drizzle-ORM 
+// SQL pages
+
+export const userSystemEnum = pgEnum('user_system_enum', ['system', 'user'])
 
 export const chats = pgTable('chats', {
     id: serial('id').primaryKey(),
@@ -7,6 +19,15 @@ export const chats = pgTable('chats', {
     pdfUrl: text('pdf_url').notNull(),
     createdAt: timestamp('created_at').notNull().defaultNow(),
     userId: varchar('user_id', {length:256}).notNull(),
-    fileKey: text('file_key').notNull()
+    fileKey: text('file_key').notNull(),
 })
 
+export const messages = pgTable('messages', {
+    id: serial('id').primaryKey(),
+    chatId: integer('cha_id').references(()=>chats.id).notNull(),
+    content: text('content').notNull(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    role: userSystemEnum('role').notNull(),
+})
+
+// Drizzle-Kit
